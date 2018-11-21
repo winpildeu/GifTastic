cbd165cb402e35252f3e852521e2430d79f99725
Winfield H <winterfreshness@gmail.com>
2018-11-20 20:31:28 -0500

    Got images to show on the screen dynamically. But the format isn't ok.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-const APIkey = "fqmMfG2eW9wEWkZyz6RpzIhI9daBM4Rb";
+const APIkey = "&api_key=fqmMfG2eW9wEWkZyz6RpzIhI9daBM4Rb&limit=10&rating=g";
 let pokedex = ["wartortle", "charizard", "mr. mime", "gengar", "psyduck", "meowth", "caterpie"];
 
 // document ready shorthand
@@ -19,8 +19,40 @@ $(function () {
         // Grab the stored value in the clicked button
         let pokemon = $(this).attr("data-name");
         console.log(`Searching for: ${pokemon}`);
-
         
+        let queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        pokemon + APIkey;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (resp) {
+            // Check that data is recieved and log it
+            console.log(queryURL);
+            console.table(resp);
+
+            // Save the useful data into a variable
+            let result = resp.data;
+            
+            // Display the images
+            for (let i = 0; i < result.length; i++) {
+                // save the title and URLs of the current image
+                let imgURL = result[i].images.fixed_height_still.url;
+                let title = result[i].title;
+                let rating = result[i].rating
+
+                // Create the card to insert into the HTML page
+                let card = $("<div>");
+                card.addClass("card m-2").attr("style", "width: 18rem;");
+                let cardImg = $("<img>");
+                cardImg.addClass("card-img-top").attr("src", imgURL).attr("alt",title).attr("style", "width: 18rem;");
+                let cardBody = $("<div>");
+                cardBody.addClass("card-body");
+                let rated = $("<p>");
+                rated.addClass("card-text").text(`Rating: ${rating}`);
+                $("#images").prepend(card, cardImg, cardBody, rated);
+            }
+        });
     }
 
     // Events =====================================
