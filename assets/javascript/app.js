da6faff7c7642acf9a910ddbd683a1ccd7d19cf7
Winfield H <winterfreshness@gmail.com>
2018-11-20 19:27:18 -0500

    Got the clicked button's value to get saved.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,40 @@
+const APIkey = "fqmMfG2eW9wEWkZyz6RpzIhI9daBM4Rb";
+let pokedex = ["wartortle", "charizard", "mr. mime", "gengar", "psyduck", "meowth", "caterpie"];
+
 // document ready shorthand
 $(function () {
-    let pokedex = ["wartortle", "charizard", "mr. mime", "gengar", "psyduck", "meowth", "caterpie"];
 
     // Functions ==================================
     function displayButtons() {
+        $("#buttons").empty();
         for (let i = 0; i < pokedex.length; i++) {
             let newButton = $("<button>");
-            newButton.attr("type", "button").attr("data-value", pokedex[i]).addClass("btn btn-secondary").text(`${pokedex[i]}`);
+            newButton.attr("type", "button").attr("data-name", pokedex[i]).addClass("btn btn-secondary pokeButton").text(`${pokedex[i]}`);
             $("#buttons").append(newButton);
         }
     }
 
+    function giphySearch() {
+
+        // Grab the stored value in the clicked button
+        let pokemon = $(this).attr("data-name");
+        console.log(`Searching for: ${pokemon}`);
+
+        
+    }
+
     // Events =====================================
+
+    // Add an onclick to all generated elements with a class of pokeButton
+    $(document).on("click", ".pokeButton", giphySearch);
+
+    // Adds a pokemon button to the list
     $("#searchButton").on("click", function () {
         event.preventDefault();
         let userInput = $("#searchField").val();
-        console.log("search button clicked.");
-        console.log(userInput);
+        console.log(`Added button: ${userInput}`);
+        pokedex.push(userInput);
+        displayButtons();
     });
 
     // Main code ==================================
